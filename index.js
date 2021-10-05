c58b9bb0447baeb4d3e4e37fec73d82e8b603abd
ArthurAtamuradov <atamuradow.arthur@gmail.com>
2021-10-05 11:28:49 +0500

    initialising routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const todoRoutes = require("./routes/todo");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
@@ -7,5 +8,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.send("/index.html");
 });
+app.use("/api/todo", todoRoutes);
 
 app.listen(PORT);
---
+++ routes/todo.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+const router = Router();
+
+router.get("/", (req, res) => {});
+
+router.post("/", (req, res) => {});
+
+router.put("/", (req, res) => {});
+
+router.delete("/", (req, res) => {});
+
+module.exports = router;
